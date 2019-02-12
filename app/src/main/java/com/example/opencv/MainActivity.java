1a22720d155dcbdf7ec18ea347bcf23d8d68a2e5
lirugo <roostik21@gmail.com>
2019-02-12 15:58:52 +0200

    detecting rectangle

--- app/src/main/java/com/example/opencv/MainActivity.java
+++ app/src/main/java/com/example/opencv/MainActivity.java
@@ -1,64 +1,89 @@
 package com.example.opencv;
 
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.SurfaceView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.androidcv.R;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    static {
-        if(!OpenCVLoader.initDebug()){
-            Log.d("TAG", "OpCV no loaded");
-        } else {
-          Log.d("TAG", "OpCv loaded");
-        }
-    }
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    int iLowH = 50;
-    int iLowS = 100;
-    int iLowV = 100;
-    int iHighH = 70;
-    int iHighS = 255;
-    int iHighV = 255;
-    Mat imgHSV, imgThresholded;
-    Scalar sc1, sc2;
-    JavaCameraView cameraView;
+    //view holder
+    CameraBridgeViewBase cameraBridgeViewBase;
+
+    //camera listener callback
+    BaseLoaderCallback baseLoaderCallback;
+
+    //image holder
+    Mat bwIMG, hsvIMG, lrrIMG, urrIMG, dsIMG, usIMG, cIMG, hovIMG;
+    MatOfPoint2f approxCurve;
+
+    int threshold;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-        sc1 = new Scalar(iLowH, iLowS, iLowV);
-        sc2 = new Scalar(iHighH, iHighS, iHighV);
-        cameraView = (JavaCameraView) findViewById(R.id.cameraview);
-        cameraView.setCameraIndex(0);
-        cameraView.setCvCameraViewListener(this);
-        cameraView.enableView();
+
+        //initialize treshold
+        threshold = 100;
+
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraViewer);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        //create camera listener callback
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS:
+                        Log.v("aashari-log", "Loader interface success");
+                        bwIMG = new Mat();
+                        dsIMG = new Mat();
+                        hsvIMG = new Mat();
+                        lrrIMG = new Mat();
+                        urrIMG = new Mat();
+                        usIMG = new Mat();
+                        cIMG = new Mat();
+                        hovIMG = new Mat();
+                        approxCurve = new MatOfPoint2f();
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
 
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        imgHSV = new Mat(width, height, CvType.CV_8UC4);
-        imgThresholded = new Mat(width, height, CvType.CV_8UC4);
+
     }
 
     @Override
@@ -68,14 +93,108 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Imgproc.cvtColor(inputFrame.rgba(), imgHSV, Imgproc.COLOR_BGR2HSV);
-        Core.inRange(imgHSV, sc1, sc2, imgThresholded);
-        return imgThresholded;
+
+        Mat gray = inputFrame.gray();
+        Mat dst = inputFrame.rgba();
+
+        Imgproc.pyrDown(gray, dsIMG, new Size(gray.cols() / 2, gray.rows() / 2));
+        Imgproc.pyrUp(dsIMG, usIMG, gray.size());
+
+        Imgproc.Canny(usIMG, bwIMG, 0, threshold);
+
+        Imgproc.dilate(bwIMG, bwIMG, new Mat(), new Point(-1, 1), 1);
+
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+
+        cIMG = bwIMG.clone();
+
+        Imgproc.findContours(cIMG, contours, hovIMG, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+        for (MatOfPoint cnt : contours) {
+
+            MatOfPoint2f curve = new MatOfPoint2f(cnt.toArray());
+
+            Imgproc.approxPolyDP(curve, approxCurve, 0.02 * Imgproc.arcLength(curve, true), true);
+
+            int numberVertices = (int) approxCurve.total();
+
+            double contourArea = Imgproc.contourArea(cnt);
+
+            if (Math.abs(contourArea) < 100) {
+                continue;
+            }
+
+            //Rectangle detected
+            if (numberVertices >= 4 && numberVertices <= 6) {
+
+                List<Double> cos = new ArrayList<>();
+
+                for (int j = 2; j < numberVertices + 1; j++) {
+                    cos.add(angle(approxCurve.toArray()[j % numberVertices], approxCurve.toArray()[j - 2], approxCurve.toArray()[j - 1]));
+                }
+
+                Collections.sort(cos);
+
+                double mincos = cos.get(0);
+                double maxcos = cos.get(cos.size() - 1);
+
+                if (numberVertices == 4 && mincos >= -0.1 && maxcos <= 0.3) {
+                    setLabel(dst, "X", cnt);
+                }
+
+            }
+
+
+        }
+
+        return dst;
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        cameraView.disableView();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "There is a problem", Toast.LENGTH_SHORT).show();
+        } else {
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null) {
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    private static double angle(Point pt1, Point pt2, Point pt0) {
+        double dx1 = pt1.x - pt0.x;
+        double dy1 = pt1.y - pt0.y;
+        double dx2 = pt2.x - pt0.x;
+        double dy2 = pt2.y - pt0.y;
+        return (dx1 * dx2 + dy1 * dy2) / Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+    }
+
+    private void setLabel(Mat im, String label, MatOfPoint contour) {
+        int fontface = Core.FONT_HERSHEY_SIMPLEX;
+        double scale = 3;//0.4;
+        int thickness = 3;//1;
+        int[] baseline = new int[1];
+        Size text = Core.getTextSize(label, fontface, scale, thickness, baseline);
+        Rect r = Imgproc.boundingRect(contour);
+        Point pt = new Point(r.x + ((r.width - text.width) / 2),r.y + ((r.height + text.height) / 2));
+        Core.putText(im, label, pt, fontface, scale, new Scalar(255, 0, 0), thickness);
+    }
+
 }
